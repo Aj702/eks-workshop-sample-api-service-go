03c5bbb33c0de514eaa8a77f7e0945236f84e080
AJ702 <42333329+Aj702@users.noreply.github.com>
2022-08-29 12:41:54 -0700

    Update main.go
    Changed hello world to Bugcrowd is awesome.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Bugcrowd Is Awesome"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
